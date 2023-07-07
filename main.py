31151128fc1a43dcef6449a597476bdb9be83b6f
MarecoAJ <alejo_mareco@hotmail.com>
2023-07-07 11:07:53 -0300

    uso de post

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
 from fastapi.responses import HTMLResponse
 
 app = FastAPI()
@@ -42,3 +42,15 @@ def get_movie(id: int):
 @app.get("/movies/", tags=["movies"])
 def get_movies_by_category(category: str):
     return list(filter(lambda item: item['category'] == category , movies))
+
+@app.post("/movies", tags=["movies"])
+def create_movie(id: int =Body(), title: str =Body(), overview: str =Body(), year: int =Body(), rating:float =Body(), category:str =Body()):
+    movies.append({
+        "id": id,
+        "title": title,
+        "overview": overview,
+        "year": year,
+        "rating": rating,
+        "category": category
+    })
+    return movies
